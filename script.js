7f732d3cce8b76b2fd4587e8060bed2d562b78ab
Nikolas Júlio <nikolasjuliogarciadejesus@gmail.com>
2023-05-02 19:34:24 -0300

    Comecei a área de galeria

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function myFunction() {
-    var x = document.getElementById("meuTitulo");
-    if (x.className === "titulo") {
-      x.className += " responsive";
-    } else {
-      x.className = "titulo";
-    }
+  var x = document.getElementById("meuTitulo");
+  if (x.className === "titulo") {
+    x.className += " responsive";
+  } else {
+    x.className = "titulo";
   }
+}
+
+function abrirGaleria(evt, nomeDaCidade) {
+  let i, tabcontent, tablinks
+
+  tabcontent = document.getElementsByClassName("tabcontent")
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none"
+  }
+
+  tablinks = document.getElementsByClassName("tablinks")
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("ativo", "")
+  }
+
+  document.getElementById(nomeDaCidade).style.display = "block"
+  evt.currentTarget.className += " ativo"
+}

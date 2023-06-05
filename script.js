2a0c2273c8ac83a83395eb950196dff81dea448e
TorchHollow <nikolasjuliogarciadejesus@gmail.com>
2023-06-05 14:49:28 -0300

    Coloquei uma image na home, e adicionei animações

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function myFunction() {
-  var x = document.getElementById("meuTitulo");
+//Responsividade da barra de menu
+
+function menuBar() {
+  let x = document.getElementById("meuTitulo");
   if (x.className === "titulo") {
     x.className += " responsive";
   } else {
@@ -7,3 +9,22 @@ function myFunction() {
   }
 }
 
+//animação do scroll
+
+function revelacao(){
+  const revelacoes = document.querySelectorAll(".revelacao")
+  for (let i = 0; i < revelacoes.length; i++){
+      let windowHeight = window.innerHeight
+      let elementoDoTopo = revelacoes[i].getBoundingClientRect().top
+      let elementoVisivel = 150
+      if (elementoDoTopo < windowHeight - elementoVisivel){
+          revelacoes[i].classList.add("ativo")
+      }else {
+          revelacoes[i].classList.remove("ativo")
+      }
+  }
+}
+
+window.addEventListener("scroll", revelacao)
+
+revelacao()
